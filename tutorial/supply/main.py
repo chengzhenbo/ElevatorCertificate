2623e7611e63ccc41edca9f793bea884c0840291
chengzhenbo <50646861+chengzhenbo@users.noreply.github.com>
2023-07-30 20:55:26 +0800

    new supply api

--- tutorial/supply/main.py
+++ tutorial/supply/main.py
@@ -4,7 +4,7 @@ from excel_reader import read_supplier_data, SupplierType
 
 def main():
 
-    product_parts = read_supplier_data(supplier_type=SupplierType.XIANSUQI, 
+    product_parts = read_supplier_data(supplier_type='da', 
                                        file = "供应商数据表/限速器缓冲器数据表.xlsx")
 
     for _, row in product_parts.iterrows():
