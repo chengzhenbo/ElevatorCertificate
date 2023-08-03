49c4b8511896d763befe8bf5d334f39631fa5d17
chengzhenbo <50646861+chengzhenbo@users.noreply.github.com>
2023-08-03 16:32:59 +0800

    modified:   tutorial/supply/main.py

--- tutorial/supply/main.py
+++ tutorial/supply/main.py
@@ -4,11 +4,15 @@ from excel_reader import read_supplier_data, SupplierType
 
 def main():
 
-    product_parts = read_supplier_data(supplier_type=SupplierType.XIANSUQI, 
-                                       file = "供应商数据表/限速器缓冲器数据表.xlsx")
-
+    product_parts = read_supplier_data(supplier_type=SupplierType.ZHUBAN_SMART, 
+                                       file = "供应商数据表/主板数据表.xlsx")
+    ind_columns = product_parts.columns.values
+    # 显示索引名
+    print('ind_columns = ', ind_columns)
+    # 遍历行
     for _, row in product_parts.iterrows():
-        print(row['HeTongHao'], row['BianHao1'])
+        # 第0列和最后一列的所有数据
+        print(row[0], row[-1])
 
 
 if __name__ == '__main__':
