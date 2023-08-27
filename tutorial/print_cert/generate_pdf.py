f73d5e29c589865002d5962751c9c55f28a246c6
chengzhenbo <50646861+chengzhenbo@users.noreply.github.com>
2023-08-27 21:00:59 +0800

    modified:   tutorial/.DS_Store 	modified:   tutorial/export_excel/template/.DS_Store 	modified:   tutorial/export_excel/template/elevator/congifuration.xlsx 	renamed:    "tutorial/export_excel/template/temp/\345\220\210\346\240\274\350\257\201\346\211\223\345\215\260\346\240\274\345\274\217 2.xlsm" -> "tutorial/export_excel/template/initial_file/\345\220\210\346\240\274\350\257\201\346\211\223\345\215\260\346\240\274\345\274\217 2.xlsm" 	renamed:    "tutorial/export_excel/template/temp/\345\220\210\346\240\274\350\257\201\346\211\223\345\215\260\346\240\274\345\274\217 2.xlsx" -> "tutorial/export_excel/template/initial_file/\345\220\210\346\240\274\350\257\201\346\211\223\345\215\260\346\240\274\345\274\217 2.xlsx" 	renamed:    "tutorial/export_excel/template/temp/\346\211\266\346\242\257\346\226\260\346\243\200\350\247\204\345\220\210\346\240\274\350\257\201.xlsx" -> "tutorial/export_excel/template/initial_file/\346\211\266\346\242\257\346\226\260\346\243\200\350\247\204\345\220\210\346\240\274\350\257\201.xlsx" 	new file:   tutorial/print_cert/.DS_Store 	new file:   tutorial/print_cert/congfig_printed_file.ini 	new file:   tutorial/print_cert/generate_pdf.py 	new file:   tutorial/print_cert/poetry.lock 	new file:   tutorial/print_cert/pyproject.toml 	new file:   tutorial/print_cert/templatefile/.DS_Store 	new file:   tutorial/print_cert/templatefile/fangzhenhei.TTF 	new file:   tutorial/print_cert/templatefile/fangzhenkai.TTF 	new file:   tutorial/print_cert/templatefile/report_template.prep 	new file:   tutorial/print_cert/templatefile/report_template.pyc 	new file:   tutorial/print_cert/templatefile/xizhi_estamp.png 	new file:   tutorial/print_cert/templatefile/xizhi_jian.png 	new file:   tutorial/print_cert/templatefile/xizhilogo.png 	new file:   tutorial/print_cert/tempoutput/.DS_Store 	new file:   tutorial/print_cert/tempoutput/certi_file.pdf 	deleted:    "tutorial/supply/tests/supplier_excel_data/~$\351\227\250\347\263\273\347\273\237\346\225\260\346\215\256\350\241\250.xlsx"

---
+++ tutorial/print_cert/generate_pdf.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+
+from z3c.rml import rml2pdf
+from pathlib import Path
+HERE = Path(__file__).resolve().parent
+import configparser
+
+import preppy 
+
+HERE = Path(__file__).resolve().parent
+
+config = configparser.ConfigParser()
+config.read(HERE / 'congfig_printed_file.ini')
+
+template_file = Path(HERE / 
+                     config.get('paths', 'template_directory') / 
+                     config.get('filenames', 'certi_file'))
+
+def generate_report(output_path):
+    mymodule = preppy.getModule(template_file)
+
+    report_data = {
+        "title": "Ad9u9231",
+        "description": "This is a summary of the product sales.",
+        "products": [
+            {"name": "Product A", "quantity": 100, "price": 15},
+            {"name": "Product B", "quantity": 50, "price": 20},
+            {"name": "Product C", "quantity": 75, "price": 12},
+        ],
+    }
+
+    rmlText = mymodule.get(report_data)
+    pdf = rml2pdf.parseString(rmlText)
+    with open(output_path, 'wb') as file:
+        file.write(pdf.read())
+
+if __name__ == "__main__":
+    output_pdf = Path(HERE / 
+                     config.get('paths', 'output_directory') / 
+                     config.get('filenames', 'output_certi_file'))
+    generate_report(output_pdf)
+
+
