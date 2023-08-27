35c674ce7fe2985dc650f5f13963fc6785f61f2c
chengzhenbo <50646861+chengzhenbo@users.noreply.github.com>
2023-08-27 22:48:52 +0800

    add export pdf

--- tutorial/print_cert/generate_pdf.py
+++ tutorial/print_cert/generate_pdf.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from z3c.rml import rml2pdf
-from pathlib import Path
-HERE = Path(__file__).resolve().parent
 import configparser
+from pathlib import Path
+import json
+import uuid
 
+from PyPDF2 import PdfReader, PdfWriter
+from z3c.rml import rml2pdf
 import preppy 
 
 HERE = Path(__file__).resolve().parent
@@ -11,31 +13,61 @@ HERE = Path(__file__).resolve().parent
 config = configparser.ConfigParser()
 config.read(HERE / 'congfig_printed_file.ini')
 
-template_file = Path(HERE / 
+
+
+template_certfile = Path(HERE / 
                      config.get('paths', 'template_directory') / 
-                     config.get('filenames', 'certi_file'))
+                     config.get('filenames', 'cert_template_file'))
 
-def generate_report(output_pdf, report_data):
-    mymodule = preppy.getModule(template_file)
+def generate_report(report_data:json)->Path:
+    output_path = Path(HERE / 
+                     config.get('paths', 'output_directory'))
+    file_name = str(uuid.uuid4()) + '.pdf'
+    out_pdf = Path(output_path / file_name)
 
+    mymodule = preppy.getModule(template_certfile)
     rmlText = mymodule.get(report_data)
     pdf = rml2pdf.parseString(rmlText)
-    with open(output_pdf, 'wb') as file:
+    with open(out_pdf, 'wb') as file:
         file.write(pdf.read())
 
-if __name__ == "__main__":
-    output_pdf = Path(HERE / 
-                     config.get('paths', 'output_directory') / 
-                     config.get('filenames', 'output_certi_file'))
-    report_data = {
-        "title": "Ad9u9231",
-        "description": "This is a summary of the product sales.",
-        "products": [
-            {"name": "Product A", "quantity": 100, "price": 15},
-            {"name": "Product B", "quantity": 50, "price": 20},
-            {"name": "Product C", "quantity": 75, "price": 12},
-        ],
-    }
-    generate_report(output_pdf, report_data)
+    if out_pdf.exists():
+        return out_pdf
+    else:
+        return None
+    
+def merge_pdfs(pdf_files:list[Path], output_path:Path)->Path:
+    pdf_writer = PdfWriter()
+    for pdf_file in pdf_files:
+        pdf_reader = PdfReader(pdf_file, strict=False)
+        for page in range(len(pdf_reader.pages)):
+            pdf_page = pdf_reader.pages[page]
+            pdf_writer.add_page(pdf_page)
+
+    # 将合并后的PDF写入文件
+    with open(output_path, "wb") as output_file:
+        pdf_writer.write(output_file)
+    
+    if output_path.exists():
+        return output_path
+    else:
+        return None
+
+def generate_reports(*json_args)->Path:
+    pdf_files = []
+    for json_str in json_args:
+        json_data = json.loads(json_str)
+        pdf_file = generate_report(json_data)
+        pdf_files.append(pdf_file)
+    
+    if len(pdf_files):
+        output_fn = merge_pdfs(pdf_files, Path("output.pdf"))
+    
+    return output_fn
+
+        
+
+
+
 
 
---
+++ tutorial/print_cert/test.py
@@ -0,0 +1,25 @@
+from generate_pdf import generate_report,generate_reports
+
+def test_one_pdf():
+    
+    report_data = {
+        "title": "Ad9u9231",
+        "description": "This is a summary of the product sales.",
+        "products": [
+            {"name": "Product A", "quantity": 100, "price": 15},
+            {"name": "Product B", "quantity": 50, "price": 20},
+            {"name": "Product C", "quantity": 75, "price": 12},
+        ],
+    }
+    pdf_file = generate_report(report_data)
+    print(pdf_file)
+
+def test_many_pdf():
+    
+    report_data1 = '{"title": "ERasdf394", "age": 30}'
+    report_data2 = '{"title": "DDAlice23", "age": 30}'
+
+    pdf_file = generate_reports(report_data1,report_data2)
+    print(pdf_file)
+if __name__ == "__main__":
+    test_many_pdf()
