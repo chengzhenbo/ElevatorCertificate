c009639d8515e1ebd0e790d0f77887fbf040ce9b
chengzhenbo <50646861+chengzhenbo@users.noreply.github.com>
2023-08-30 10:52:45 +0800

    modified:   tutorial/print_cert/__pycache__/generate_pdf.cpython-311.pyc 	new file:   tutorial/print_cert/cert_print.py 	modified:   tutorial/print_cert/generate_pdf.py 	new file:   tutorial/print_cert/json1.txt 	new file:   "tutorial/print_cert/json2\346\235\241.txt" 	modified:   tutorial/print_cert/templatefile/cert_template.prep 	modified:   tutorial/print_cert/templatefile/cert_template.pyc 	modified:   tutorial/print_cert/tempoutput/test.pdf 	modified:   tutorial/print_cert/test.py

--- tutorial/print_cert/test.py
+++ tutorial/print_cert/test.py
@@ -1,16 +1,20 @@
-from generate_pdf import generate_report,generate_reports
+from generate_pdf import generate_report,generate_reports,extract_data
+from pathlib import Path
+import json
+
+HERE = Path(__file__).resolve().parent
+
+def test_json():
+    with open(Path(HERE/"json1.txt")) as f:
+        json_data = json.load(f)
+    print(json_data)
+    
 
 def test_one_pdf():
-    
-    report_data = {
-        "title": "Ad9u9231",
-        "description": "This is a summary of the product sales.",
-        "products": [
-            {"name": "Product A", "quantity": 100, "price": 15},
-            {"name": "Product B", "quantity": 50, "price": 20},
-            {"name": "Product C", "quantity": 75, "price": 12},
-        ],
-    }
+    with open(Path(HERE/"json1.txt")) as f:
+        json_data = json.load(f)
+    report_data = extract_data(json_data[0])
+
     pdf_file = generate_report(report_data)
     print(pdf_file)
 
@@ -18,8 +22,10 @@ def test_many_pdf():
     
     report_data1 = '{"title": "ERasdf394", "age": 30}'
     report_data2 = '{"title": "DDAlice23", "age": 30}'
-
+   
     pdf_file = generate_reports(report_data1,report_data2)
     print(pdf_file)
+
+
 if __name__ == "__main__":
     test_one_pdf()
